2e01f0336942a173240b500f38eed4e0dc5e2021
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-01-11 12:40:00 +0000

    new search

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['next'],
+  extends: ['eslint:recommended', 'next', 'next/core-web-vitals'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
