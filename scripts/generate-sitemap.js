cd13a87d995824b59a3876f4dc2b00362931d71e
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-01-23 21:02:47 +0000

    prettire config changes

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,21 +1,19 @@
-const fs = require('fs')
-const globby = require('globby')
-const matter = require('gray-matter')
-const prettier = require('prettier')
-const siteMetadata = require('../data/siteMetadata')
+const fs = require('fs');
+const globby = require('globby');
+const matter = require('gray-matter');
+const prettier = require('prettier');
+const siteMetadata = require('../data/siteMetadata');
 
-;(async () => {
-	const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
-	const pages = await globby([
-		'pages/*.js',
+(async () => {
+	const prettierConfig = await prettier.resolveConfig('./.prettierrc.js');
+	const pages = await globby(['pages/*.js',
 		'pages/*.tsx',
 		'data/blog/**/*.mdx',
 		'data/blog/**/*.md',
 		'public/tags/**/*.xml',
 		'!pages/_*.js',
 		'!pages/_*.tsx',
-		'!pages/api',
-	])
+		'!pages/api']);
 
 	const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
@@ -24,44 +22,36 @@ const siteMetadata = require('../data/siteMetadata')
 		.map((page) => {
 			// Exclude drafts from the sitemap
 			if (page.search('.md') >= 1 && fs.existsSync(page)) {
-				const source = fs.readFileSync(page, 'utf8')
-				const fm = matter(source)
+				const source = fs.readFileSync(page, 'utf8');
+				const fm = matter(source);
 				if (fm.data.draft) {
-					return
+					return;
 				}
 				if (fm.data.canonicalUrl) {
-					return
+					return;
 				}
 			}
-			const path = page
-				.replace('pages/', '/')
-				.replace('data/blog', '/blog')
-				.replace('public/', '/')
-				.replace('.js', '')
-				.replace('.tsx', '')
-				.replace('.mdx', '')
-				.replace('.md', '')
-				.replace('/feed.xml', '')
-			const route = path === '/index' ? '' : path
+			const path = page.replace('pages/', '/').replace('data/blog', '/blog').replace('public/', '/').replace('.js', '').replace('.tsx', '').replace('.mdx', '').replace('.md', '').replace('/feed.xml', '');
+			const route = path === '/index' ? '' : path;
 
 			if (page.search('pages/404.') > -1 || page.search('pages/blog/[...slug].') > -1) {
-				return
+				return;
 			}
 			return `
                         <url>
                             <loc>${siteMetadata.siteUrl}${route}</loc>
                         </url>
-                    `
+                    `;
 		})
 		.join('')}
         </urlset>
-    `
+    `;
 
 	const formatted = prettier.format(sitemap, {
 		...prettierConfig,
 		'parser': 'html',
-	})
+	});
 
 	// eslint-disable-next-line no-sync
-	fs.writeFileSync('public/sitemap.xml', formatted)
-})()
+	fs.writeFileSync('public/sitemap.xml', formatted);
+})();
