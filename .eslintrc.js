7dde1c1532813d2288765c89277b9a6ed91eee73
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-01-23 15:53:46 +0000

    prettire config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,21 @@ module.exports = {
     node: true,
     es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended', 'next', 'next/core-web-vitals'],
+  extends: ['eslint:recommended', 'next', 'next/core-web-vitals'],
   rules: {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 0,
     'no-unused-vars': 0,
     'react/no-unescaped-entities': 0,
+    quotes: ['error', 'single'],
+    'quote-props': ['error', 'always'],
+    indent: ['error', 'tab'],
+    'array-element-newline': [
+      'error',
+      {
+        multiline: true,
+        minItems: 5,
+      },
+    ],
   },
 }
