62f17a4b2c2ba0de9885acfad14dd83cadbc6158
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-01-23 15:54:46 +0000

    prettire config changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    amd: true,
-    node: true,
-    es6: true,
-  },
-  extends: ['eslint:recommended', 'next', 'next/core-web-vitals'],
-  rules: {
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 0,
-    'no-unused-vars': 0,
-    'react/no-unescaped-entities': 0,
-    quotes: ['error', 'single'],
-    'quote-props': ['error', 'always'],
-    indent: ['error', 'tab'],
-    'array-element-newline': [
-      'error',
-      {
-        multiline: true,
-        minItems: 5,
-      },
-    ],
-  },
+	'root': true,
+	'env': {
+		'browser': true,
+		'amd': true,
+		'node': true,
+		'es6': true,
+	},
+	'extends': ['eslint:recommended', 'next', 'next/core-web-vitals'],
+	'rules': {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 0,
+		'no-unused-vars': 0,
+		'react/no-unescaped-entities': 0,
+		'quotes': ['error', 'single'],
+		'quote-props': ['error', 'always'],
+		'indent': ['error', 'tab'],
+		'array-element-newline': [
+			'error',
+			{
+				'multiline': true,
+				'minItems': 5,
+			},
+		],
+	},
 }
