9becee7185f8063cc637ecd3ac2913706bb97009
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-09-03 00:04:52 +0100

    remove old intro text

--- pages/index.js
+++ pages/index.js
@@ -20,11 +20,7 @@ export default function Home() {
             </span>
           </h1>
           <h2>{`Welcome to ${siteMetadata.description}`}</h2>
-          <h2>I am an independent software developer currently based in Singapore. I am the creator of the JavaScript framework Vue.js and the frontend build tool Vite. Most of my work is open source and publicly available on GitHub. If you happen to benefit from my OSS work, you can support me financially via GitHub Sponsors.
-
-  You can follow me on Twitter where I mostly tweet about Vue and frontend technologies. If you happen to speak Chinese, my Chinese name is 尤雨溪 (yóu yǔ xī) and I have a Chinese-only Twitter alt for non-tech-focused musings. You can also find me on 微博 and 知乎.
-
-  Outside of programming and helping my wife take care of our two kids, I enjoy video games, karaoke, sushi, watching UFC/F1, and collecting mechanical watches.</h2>
+          <h2></h2>
           <LauncherShortcut />
         </div>
         <ListLayout posts={ sortPosts(allPosts, 'date').slice(0, 5) } linkAllPosts={ true } listTitle='Latest Posts' />
