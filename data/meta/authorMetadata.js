ecfdfccbda5852d11583065a54e92a3ba5cc9f55
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2023-09-08 15:25:29 +0100

    Change homepage layout

--- data/meta/authorMetadata.js
+++ data/meta/authorMetadata.js
@@ -1,11 +1,6 @@
 const AuthorMetadata = {
-  'about': `I am an independent software developer currently based in Singapore. I am the creator of the JavaScript framework Vue.js and the frontend build tool Vite. Most of my work is open source and publicly available on GitHub. If you happen to benefit from my OSS work, you can support me financially via GitHub Sponsors.
-  
-  You can follow me on Twitter where I mostly tweet about Vue and frontend technologies. If you happen to speak Chinese, my Chinese name is 尤雨溪 (yóu yǔ xī) and I have a Chinese-only Twitter alt for non-tech-focused musings. You can also find me on 微博 and 知乎.
-  
-  Outside of programming and helping my wife take care of our two kids, I enjoy video games, karaoke, sushi, watching UFC/F1, and collecting mechanical watches.`,
   'author': 'Ahmad Assaf',
-  'description': 'Applying Aritificial Intelligence to solve complex problem',
+  'description': 'I Apply Aritificial Intelligence to solve complex problem',
   'email': 'me@assaf.website',
   'github': 'https://github.com/ahmadassaf',
   'linkedin': 'https://www.linkedin.com/in/ahmadassaf',
